bbc6b0858c44d462ab6df14055a6c77218794cc4
Karthikeya <karthikeya0800@gmail.com>
2022-11-01 15:08:14 +0530

    Filtered-react-projects

--- 03_airbnb_clone/src/data.js
+++ 03_airbnb_clone/src/data.js
@@ -16,7 +16,7 @@ export default [
         id: 2,
         title: "Learn Wedding Photography",
         description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
-        price: 125,
+        price: 120,
         coverImg: "../images/wedding-photography.png",
         stats: {
             rating: 5.0,
