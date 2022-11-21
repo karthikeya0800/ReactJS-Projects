3cbf150320eaa2efafad394709be7e95033cc386
Karthikeya <karthikeya0800@gmail.com>
2022-11-21 17:50:04 +0530

    Data-called-using-API

--- 05_meme_generator/src/components/Meme.js
+++ 05_meme_generator/src/components/Meme.js
@@ -1,24 +1,30 @@
 import React from "react"
-import memesData from "../memesData";
+// import memesData from "../memesData";
 export default function Meme() {
 
     const [meme, setMeme] = React.useState({
         topText: "",
         bottomText: "",
-        randomImage: "https://i.imgflip.com/30b1gx.jpg"
+        randomImage: ""
     })
 
-    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+    // const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+    const [allMemes,setAllMemes]=React.useState({})
+
+    React.useEffect(function (){
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data))
+    },[])
 
     function getMemeImage() {
-        const memesArray = allMemeImages.data.memes
+        const memesArray = allMemes.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        setMeme(prevStatus => ({
+        setMeme({
             topText:"",
             bottomText:"",
             randomImage: memesArray[randomNumber].url
         })
-        )
     }
 
     function handleChange(event){
@@ -59,7 +65,7 @@ export default function Meme() {
             </div>
 
             <div className="meme">
-                <img className="meme--image" src={meme.randomImage} />
+                <img className="meme--image" alt="Meme Here" src={meme.randomImage} />
                 <h2 className="meme-text top">{meme.topText}</h2>
                 <h2 className="meme-text bottom">{meme.bottomText}</h2>
             </div>
