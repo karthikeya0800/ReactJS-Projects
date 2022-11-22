ec8e9a88dd2d0c1a050be37eadb8509ddd9f2a37
Karthikeya <karthikeya0800@gmail.com>
2022-11-22 15:51:30 +0530

    p2-Added-darkmode-feature

--- 02_react_infosite/src/Header.js
+++ 02_react_infosite/src/Header.js
@@ -1,11 +1,16 @@
 import React from "react"
 
-export default function Header(){
+export default function Header(props){
     return(
     <header>
       <nav className="nav">           
           <img alt ="" className="nav-logo" src="react-logo.png"/>
-          <h3>React Facts</h3> 
+          <h3>React Facts</h3>
+          <h5>Light</h5>
+          <div class={props.mode?"toggledark":"togglelight"} onClick={props.handleMode}>
+            <div class={props.mode?"circledark":"circlelight"}></div>
+          </div>
+          <h5>Dark</h5>
           <ul className="nav-items">
             <li>Pricing</li>
             <li>About</li>
--- 02_react_infosite/src/index.js
+++ 02_react_infosite/src/index.js
@@ -5,9 +5,13 @@ import MainContent from "./MainContent"
 import Footer from "./Footer"
 
 function Page(){
+  const [darkMode,setDarkMode]=React.useState("")
+  function changeMode(){
+    setDarkMode(prev => !prev)
+  }
   return(
-    <div>
-      <Header/>
+    <div className={darkMode?"dark":""}>
+      <Header handleMode={changeMode} mode = {darkMode}/>
       <MainContent/>
       <Footer/>
     </div>
